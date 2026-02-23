3604516c7b44a13cf6ec1c4b71891c32fc5763c1
Valentin Mihov <valentin.mihov@gmail.com>
2026-02-23 13:38:17 +0200

    refactor: remove IEulerAdapter compatibility interface

--- src/interfaces/IEulerAdapter.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import {ILendingAdapter} from "./ILendingAdapter.sol";
-
-/// @dev Backward-compatible alias. Prefer ILendingAdapter.
-interface IEulerAdapter is ILendingAdapter {}
